5aeed353747c09ca4e2c79fb098091b07f3e7f57
Arshin Jain <arshin.jain@outlook.com>
2016-05-24 16:52:56 -0700

    Minor Options text modification

--- ThreeDTouch/PreviewViewController.swift
+++ ThreeDTouch/PreviewViewController.swift
@@ -10,11 +10,11 @@ class PreviewViewController: UIViewController {
     
     var previewActions:[UIPreviewActionItem] {
         
-        let item1 = UIPreviewAction(title: "Take Action", style: .Default) { (action:UIPreviewAction, UIViewController) -> Void in
+        let item1 = UIPreviewAction(title: "Share", style: .Default) { (action:UIPreviewAction, UIViewController) -> Void in
             print("Select")
         }
         
-        let item2 = UIPreviewAction(title: "Take Another Action", style: .Default) { (UIPreviewAction, UIViewController) -> Void in
+        let item2 = UIPreviewAction(title: "Save", style: .Default) { (UIPreviewAction, UIViewController) -> Void in
             print("Delete")
         }
         
@@ -24,36 +24,11 @@ class PreviewViewController: UIViewController {
     // Preview Label - Outlet
     @IBOutlet weak var previewLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
         previewLabel.text = selectedItem
         
     }
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
